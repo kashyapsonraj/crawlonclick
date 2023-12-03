fcc1467784389541d80d8f5fca260aeb970d278b
brijeshsonraj <kashyapsonraj@gmail.com>
2023-12-03 16:56:48 +0530

    update code

--- core/config.py
+++ core/config.py
@@ -1,11 +1,12 @@
-import os
-
 from dotenv import load_dotenv
 load_dotenv()
 
+from dotenv import dotenv_values
+config = dotenv_values(".env")  # config = {"USER": "foo", "EMAIL": "foo@example.org"}
+
 # MONGO SETTING
-MONGO_HOST = os.getenv("MONGO_HOST")
-MONGO_PORT = os.getenv("MONGO_HOST")
-MONGO_USER = os.getenv("MONGO_USER")
-MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
-MONGO_DB_NAME = os.getenv("MONGO_DB_NAME")
+MONGO_HOST = config.get("MONGO_HOST")
+MONGO_PORT = config.get("MONGO_PORT")
+MONGO_USER = config.get("MONGO_USER")
+MONGO_PASSWORD = config.get("MONGO_PASSWORD")
+MONGO_DB_NAME = config.get("MONGO_DB_NAME")
